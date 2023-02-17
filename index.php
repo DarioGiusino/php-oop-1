11f90840a30d9173711b593efb7f5abeedfc2fe6
DarioGiusino <dario.giusino@gmail.com>
2023-02-17 13:56:22 +0100

    defined constructor

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ class Movie
     public $title;
     public $author;
     public $language;
+
+    public function __construct($_title, $_author, $_language)
+    {
+        $this->title = $_title;
+        $this->author = $_author;
+        $this->language = $_language;
+    }
 }
