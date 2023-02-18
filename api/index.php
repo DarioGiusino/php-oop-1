6aeb2483fad4b16bc7a8d1fc94fd760bedb60412
DarioGiusino <dario.giusino@gmail.com>
2023-02-18 19:22:32 +0100

    created api folder

---
+++ api/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once __DIR__ . '/models/Movie.php';
+include_once __DIR__ . '/data/db.php';
+
+// // var_dump($movies)
+
+header('Content-Type: application/json');
+
+echo json_encode($movies);
